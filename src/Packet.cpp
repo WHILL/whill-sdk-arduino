48e8d7c0c976183be0687888b268f394e161f1cc
s-katsu <katsumata@whill.jp>
2021-08-30 15:56:24 +0900

    Apply formatter.

--- src/Packet.cpp
+++ src/Packet.cpp
@@ -24,75 +24,68 @@ THE SOFTWARE.
 
 #include "WHILL.h"
 
-WHILL::Packet::Packet(){
-  protocol_sign = 0xAF;
-  len = 1;
-  build();
+WHILL::Packet::Packet() {
+    protocol_sign = 0xAF;
+    len = 1;
+    build();
 }
 
-WHILL::Packet::Packet(unsigned char payload[],int size){
-  protocol_sign = 0xAF;
-  len = size + 1;
-  memcpy(this->payload,payload,size);
-  build();
+WHILL::Packet::Packet(unsigned char payload[], int size) {
+    protocol_sign = 0xAF;
+    len = size + 1;
+    memcpy(this->payload, payload, size);
+    build();
 }
 
-bool WHILL::Packet::is_valid(){
-  return getCalculatedCS() == cs;
+bool WHILL::Packet::is_valid() { return getCalculatedCS() == cs; }
+
+int WHILL::Packet::rawLength() {
+    return 2 + len;  // protocol_sign + len + (the length of payload and cs)
 }
 
-int WHILL::Packet::rawLength(){
-  return 2 + len;   // protocol_sign + len + (the length of payload and cs)
-}
-
-bool WHILL::Packet::setRaw(unsigned char* raw,int whole_length){
-
+bool WHILL::Packet::setRaw(unsigned char* raw, int whole_length) {
     protocol_sign = raw[0];
-    len           = raw[1];
+    len = raw[1];
 
     int prefix = 2;
     int i = 0;
-    for(i=0;i<len-1;i++){
-      payload[i]= raw[prefix+i];
+    for (i = 0; i < len - 1; i++) {
+        payload[i] = raw[prefix + i];
     }
 
-    cs = raw[prefix+i];
-    
+    cs = raw[prefix + i];
+
     return is_valid();
 }
 
-unsigned char WHILL::Packet::getCalculatedCS(){
-  unsigned char cs = 0x00;
+unsigned char WHILL::Packet::getCalculatedCS() {
+    unsigned char cs = 0x00;
 
-  cs ^= protocol_sign;
-  cs ^= len;
+    cs ^= protocol_sign;
+    cs ^= len;
 
-  for(int i=0;i<len-1;i++){
-    cs ^= payload[i];
-  }
+    for (int i = 0; i < len - 1; i++) {
+        cs ^= payload[i];
+    }
 
-  return cs;
+    return cs;
 }
 
-void WHILL::Packet::build(){
-  this->cs = getCalculatedCS();
+void WHILL::Packet::build() { this->cs = getCalculatedCS(); }
+
+int WHILL::Packet::getRaw(unsigned char* raw) {
+    int whole_length = 0;
+
+    raw[0] = protocol_sign;
+    raw[1] = len;
+
+    int prefix = 2;
+    int i = 0;
+    for (i = 0; i < len - 1; i++) {
+        raw[prefix + i] = payload[i];
+    }
+
+    raw[prefix + i] = cs;
+
+    return rawLength();
 }
-
-int WHILL::Packet::getRaw(unsigned char* raw){
-
-  int whole_length = 0;
-
-  raw[0] = protocol_sign;
-  raw[1] = len;
-
-  int prefix = 2;
-  int i = 0;
-  for(i=0;i<len-1;i++){
-    raw[prefix+i] = payload[i];
-  }
-
-  raw[prefix+i] = cs;
-  
-  return rawLength();
-}
-
--- src/WHILL.h
+++ src/WHILL.h
@@ -30,79 +30,68 @@ THE SOFTWARE.
 
 class SoftwareSerial;
 
+class WHILL {
+    class Packet {
+       private:
+       public:
+        const static unsigned char PROTOCOL_SIGN = 0xAF;
 
-class WHILL{
+        const static int MAX_LENGTH = 35;
+        const static int MAX_PAYLOAD = MAX_LENGTH - 3;  // protocol_sign,len,cs
 
-    class Packet{
+        Packet();
+        Packet(unsigned char payload[], int size);
 
-        private:
+        unsigned char getCalculatedCS();
 
-        public:
-            const static unsigned char PROTOCOL_SIGN = 0xAF;
+        unsigned char protocol_sign;
+        unsigned char len;
+        unsigned char payload[MAX_LENGTH];
+        unsigned char cs;
 
-            const static int MAX_LENGTH   = 35;
-            const static int MAX_PAYLOAD  = MAX_LENGTH - 3;  // protocol_sign,len,cs
+        bool is_valid();
 
-            Packet();
-            Packet(unsigned char payload[],int size);
+        int rawLength();
 
-            unsigned char       getCalculatedCS();
-
-            unsigned char       protocol_sign;
-            unsigned char       len;
-            unsigned char       payload[MAX_LENGTH];
-            unsigned char       cs;
-
-            bool is_valid();
-
-            int  rawLength();
-
-            bool setRaw(unsigned char* raw,int len);
-            int  getRaw(unsigned char* raw);
-            void build();
+        bool setRaw(unsigned char* raw, int len);
+        int getRaw(unsigned char* raw);
+        void build();
     };
 
+    class PacketParser {
+       private:
+        WHILL* whill = NULL;
+        void parseDataset0(WHILL::Packet* packet);
+        void parseDataset1(WHILL::Packet* packet);
 
-
-    class PacketParser{
-
-        private:
-            WHILL* whill = NULL;
-            void parseDataset0(WHILL::Packet* packet);
-            void parseDataset1(WHILL::Packet* packet);
-
-        public:
-            void setParent(WHILL* whill);
-            void setWHILLReceiver(WHILL* whill);
-            int parsePacket(Packet* packet); 
-
+       public:
+        void setParent(WHILL* whill);
+        void setWHILLReceiver(WHILL* whill);
+        int parsePacket(Packet* packet);
     };
 
+    class PacketReceiver {
+       private:
+        unsigned char buf[Packet::MAX_LENGTH] = {0};
+        unsigned char index = 0;
+        bool recording = false;
 
-    class PacketReceiver{
+        void (*callback)() = NULL;
+        bool call_callback();
 
-        private:
-            unsigned char buf[Packet::MAX_LENGTH]   = {0};
-            unsigned char index     = 0;
-            bool          recording = false;
+        PacketParser* obj = NULL;
+        int (PacketParser::*method)(WHILL::Packet* packet) = NULL;
 
-            void(*callback)()       = NULL;
-            bool          call_callback();
-
-            PacketParser* obj       = NULL;
-            int (PacketParser::*method)(WHILL::Packet* packet) = NULL;
-
-        public:
-            int push(unsigned char data);
-            int remaining_bytes();
-            void reset();
-            void register_callback(void (*callback)());
-            void register_callback(PacketParser* obj,int(PacketParser::*method)(WHILL::Packet* packet));
+       public:
+        int push(unsigned char data);
+        int remaining_bytes();
+        void reset();
+        void register_callback(void (*callback)());
+        void register_callback(PacketParser* obj, int (PacketParser::*method)(
+                                                      WHILL::Packet* packet));
     };
 
-
-
-private:
+   private:
     SoftwareSerial* serial;
 
     // Custom One byte transceiver
@@ -113,24 +102,22 @@ private:
     void transferPacket(Packet* packet);
 
     PacketReceiver receiver;
-    PacketParser   parser;
+    PacketParser parser;
 
-
-public:
-
+   public:
     WHILL(SoftwareSerial* ss);
     void begin(unsigned int interval);
- 
-    //Callback
-    enum EVENT{
+
+    // Callback
+    enum EVENT {
         CALLBACK_DATA0,
         CALLBACK_DATA1,
         CALLBACK_POWER_ON,
         EVENT_SIZE
-    }; 
+    };
     typedef void (*Callback)(WHILL*);
     Callback callback_functions[EVENT_SIZE] = {NULL};
-    void register_callback(Callback method,EVENT event);
+    void register_callback(Callback method, EVENT event);
     void fire_callback(EVENT event);
 
     void refresh();
@@ -138,7 +125,7 @@ public:
     void keep_joy_delay(unsigned long ms);
     void delay(unsigned long ms);
 
-    typedef struct{
+    typedef struct {
         unsigned char forward_spped;
         unsigned char forward_acceleration;
         unsigned char forward_deceleration;
@@ -149,50 +136,48 @@ public:
 
         unsigned char turn_speed;
         unsigned char turn_acceleration;
-        unsigned char turn_deceleration;       
-    }SpeedProfile;
+        unsigned char turn_deceleration;
+    } SpeedProfile;
 
-    typedef struct{
+    typedef struct {
         int x;
         int y;
         int z;
-    }Data3D;
+    } Data3D;
 
-    typedef struct{
+    typedef struct {
         int x;
         int y;
-    }Joy;
+    } Joy;
 
-    typedef struct{
+    typedef struct {
         unsigned char level;
         signed long current;
-    }Battery;
+    } Battery;
 
-    typedef struct{
+    typedef struct {
         float angle;
         int speed;
-    }Motor;
+    } Motor;
 
-    Data3D  accelerometer = {0};
-    Data3D  gyro          = {0};
-    Joy     virtual_joy   = {0};
-    Joy     joy           = {0};
-    Battery battery       = {0};
-    Motor   left_motor    = {0};
-    Motor   right_motor   = {0};
-    bool    power         = false;
-    int     speed_mode_indicator = -1;
+    Data3D accelerometer = {0};
+    Data3D gyro = {0};
+    Joy virtual_joy = {0};
+    Joy joy = {0};
+    Battery battery = {0};
+    Motor left_motor = {0};
+    Motor right_motor = {0};
+    bool power = false;
+    int speed_mode_indicator = -1;
 
-    //WHILL commands
-    void startSendingData0(unsigned int interval_ms,unsigned char speed_mode);
+    // WHILL commands
+    void startSendingData0(unsigned int interval_ms, unsigned char speed_mode);
     void startSendingData1(unsigned int interval_ms);
     void stopSendingData();
-    void setJoystick(int x,int y);
+    void setJoystick(int x, int y);
     void setPower(bool power);
     void setBatteryVoltaegeOut(bool out);
-    void setSpeedProfile(SpeedProfile* profile,unsigned char speed_mode);
-
+    void setSpeedProfile(SpeedProfile* profile, unsigned char speed_mode);
 };
-
 
 #endif
